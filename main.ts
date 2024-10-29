2879f169e3433f7f1dc98c316363c00a17c35c30
quorafind <951011105@qq.com>
2024-10-29 09:50:19 +0800

    chore: support bon workflow

--- main.ts
+++ main.ts
@@ -1,13 +1,47 @@
-import { Plugin } from 'obsidian';
+import { Menu, Plugin, TAbstractFile, TFolder, WorkspaceLeaf } from "obsidian";
 
-export default class MyPlugin extends Plugin {
-
+export default class BonWorkflow extends Plugin {
 	async onload() {
-
+		this.registerEvent(
+			this.app.workspace.on("file-menu", this.onFileMenu.bind(this))
+		);
 	}
 
-	onunload() {
+	onunload() {}
 
+	onFileMenu(
+		menu: Menu,
+		file: TAbstractFile,
+		source: string,
+		leaf?: WorkspaceLeaf
+	) {
+		menu.addItem((item) => {
+			item.setIcon("search")
+				.setTitle("Search in selected folder")
+				.onClick(() => {
+					const leaf =
+						this.app.workspace.getLeavesOfType("search")[0];
+					if (leaf && leaf.isDeferred) {
+						leaf.loadIfDeferred();
+					}
+					const viewState = leaf?.getViewState();
+					this.app.workspace.revealLeaf(leaf);
+					leaf?.setViewState({
+						...viewState,
+						active: true,
+					});
+					leaf?.view.setState(
+						{
+							query:
+								file instanceof TFolder
+									? `path:"${file.path}/"`
+									: `path:"${file.path}"`,
+						},
+						{
+							history: false,
+						}
+					);
+				});
+		});
 	}
-
 }
